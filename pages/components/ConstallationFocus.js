bb5b4d2bb3f480c22075310ec967a331020f3267
patleserdev <leserpat@hotmail.com>
2025-03-02 19:52:36 +0100

    repair planetfocus

--- pages/components/ConstallationFocus.js
+++ pages/components/ConstallationFocus.js
@@ -1,6 +1,6 @@
 export default function ConstellationFocus({ setFocus }) {
     return (
-      <div style={{padding:10,display:'flex',flexDirection:'column',alignItems:'flex-start',gap:5, position: "absolute", top: 20, left: 20 ,color:'white',backgroundColor:'rgba(255,255,255,0.2',zIndex:5000 }}>
+      <div style={{padding:10,display:'flex',flexDirection:'column',alignItems:'flex-start',gap:5, position: "absolute", top: 20, left: 20 ,color:'white',backgroundColor:'rgba(255,255,255,0.2)',zIndex:5000 }}>
         <button onClick={() => setFocus("Orion")}>Voir Orion</button>
         <button onClick={() => setFocus("Grande Ourse")}>Voir Grande Ourse</button>
       </div>
