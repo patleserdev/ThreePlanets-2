5e656315d42dd43fe1fb6045f015e6f7584f4244
patleserdev <leserpat@hotmail.com>
2025-03-28 19:54:49 +0100

    add navbar toggler

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,7 @@ import Image from "next/image.js"
 import styles from "../styles/navbar.module.css"
 import Link from "next/link.js"
 import { useState } from "react";
+import BurgerIcon from "./BurgerIcon";
 
 export default function Navbar()
 {
@@ -14,6 +15,9 @@ export default function Navbar()
     return (
         <nav className={styles.navbar}>
             <Image src={"/pictures/logo.png"} alt="logo" width={100} height={100} className={styles.logo}/>
+
+        
+        <div className={styles.desktop}>
         <ul>
           <li>
             <Link href="/">Accueil</Link>
@@ -28,6 +32,31 @@ export default function Navbar()
             <Link href="/quizz">Quizz</Link>
           </li>
         </ul>
+        
+        </div>
+
+        <div className={styles.mobile}>
+        <div style={{width:40,height:40}} onClick={()=>handleClick()}><BurgerIcon/></div>
+        </div>
+        <div className={open ? styles.toggleOpen : styles.toggleClose  }>
+       { open && <div> <ul>
+          <li>
+            <Link onClick={handleClick} href="/">Accueil</Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href="/map">Carte du système</Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href="/planets">Les planètes</Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href="/quizz">Quizz</Link>
+          </li>
+        </ul></div> }
+        
+        </div>
+        
+
       </nav>
       
     )
